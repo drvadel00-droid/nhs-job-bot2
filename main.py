58b9190bc1c5e2bff6375c90832a7fef386a1e9c
drvadel00-droid <drvadel00@gmail.com>
2026-02-23 09:07:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ URLS = [
     # HealthJobsUK
     "https://www.healthjobsuk.com/job_list?JobSearch_q=&JobSearch_d=&JobSearch_g=&JobSearch_re=_POST&JobSearch_re_0=1&JobSearch_re_1=1-_-_-&JobSearch_re_2=1-_-_--_-_-&JobSearch_Submit=Search&_tr=JobSearch&_ts=302",
 
+    "https://www.healthjobsuk.com/job_list?JobSearch_q=&JobSearch_d=&JobSearch_g=&JobSearch_re=_POST&JobSearch_re_0=1&JobSearch_re_1=1-_-_-&JobSearch_re_2=1-_-_--_-_-&JobSearch_Submit=Search&_tr=JobSearch&_ts=20244&_srt=startdate&_sd=al,
+    
     # NHS Jobs England
     "https://www.jobs.nhs.uk/candidate/search/results?keyword=doctor&sort=publicationDateDesc",
 
